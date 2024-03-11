06d45b4b1e84a9e6a543cf3f2483ff38eddc255b
Sehwan Yoo <krstyle03v@gmail.com>
2024-03-11 15:39:48 +0800

    Change data directory and import

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ MODEL_FLAGS="--image_size 64 --num_channels 128 --num_res_blocks 3 --learn_sigma
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 128"
 
-data_dir = '/home/yoos-bii/Desktop/data_tct/train'
+data_dir = 'W:/Sehwan/TCT_classificaion/data_tct/train'
 
-command = f'python3 scripts/image_train.py --data_dir {data_dir} {MODEL_FLAGS} {DIFFUSION_FLAGS} {TRAIN_FLAGS}'
+command = f'python scripts/image_train.py --data_dir {data_dir} {MODEL_FLAGS} {DIFFUSION_FLAGS} {TRAIN_FLAGS}'
 
 os.system(command)
